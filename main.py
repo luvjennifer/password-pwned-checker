d9c752bd0e307bf95599080283570799846ccb6f
jenniferhuang <jenniferhuang@jenniferdeMacBook-Pro.local>
2021-10-06 11:29:49 +0800

    modulize code

--- main.py
+++ main.py
@@ -8,46 +8,50 @@ def get_input():
     return getpass.getpass('your desired password, [q] to quit > ')
 
 
+def check_password(real_pwd):
+    while real_pwd.lower() != 'q':
+        # check on user input
+        if len(real_pwd) == 0:
+            print("your password is empty!")
+            real_pwd = get_input()
+            continue
+
+        # check if its pwned
+        # 1. get hashed real-pwd and the first 5 digit
+        hashed_pwd = hashlib.sha1(
+            real_pwd.encode('utf-8')).hexdigest().upper()
+        first_5_hashed_pwd = hashed_pwd[0:5]
+
+        # 2. send to api and retrieve same prefix data set
+        pwd_dict = {}
+        try:
+            ni = NetworkInterface()
+            res = ni.get('https://api.pwnedpasswords.com/range/',
+                         first_5_hashed_pwd)
+        except:
+            # display only : by network module
+            exit()
+        else:
+            pwd_dict = {item.split(':')[0]: item.split(':')[1]
+                        for item in res.text.split('\r\n')}
+            # print(pwd_dict)
+
+        # show the result to user
+        if pwd_dict.get(hashed_pwd[5:]) is not None:
+            print(
+                f'the passowrd has been pwned for {pwd_dict.get(hashed_pwd[5:])} times')
+        else:
+            print('this password is safe!')
+
+        real_pwd = get_input()
+
+
 def main():
     try:
         # get password from user
         real_pwd = get_input()
 
-        while real_pwd.lower() != 'q':
-            # check on user input
-            if len(real_pwd) == 0:
-                print("your password is empty!")
-                real_pwd = get_input()
-                continue
-
-            # check if its pwned
-            # 1. get hashed real-pwd and the first 5 digit
-            hashed_pwd = hashlib.sha1(
-                real_pwd.encode('utf-8')).hexdigest().upper()
-            first_5_hashed_pwd = hashed_pwd[0:5]
-
-            # 2. send to api and retrieve same prefix data set
-            pwd_dict = {}
-            try:
-                ni = NetworkInterface()
-                res = ni.get('https://api.pwnedpasswords.com/range/',
-                             first_5_hashed_pwd)
-            except:
-                # display only : by network module
-                exit()
-            else:
-                pwd_dict = {item.split(':')[0]: item.split(':')[1]
-                            for item in res.text.split('\r\n')}
-                # print(pwd_dict)
-
-            # show the result to user
-            if pwd_dict.get(hashed_pwd[5:]) is not None:
-                print(
-                    f'the passowrd has been pwned for {pwd_dict.get(hashed_pwd[5:])} times')
-            else:
-                print('this password is safe!')
-
-            real_pwd = get_input()
+        check_password(real_pwd)
     except KeyboardInterrupt:
         print('\r\n user exit')
         return
